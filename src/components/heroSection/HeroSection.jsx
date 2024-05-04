7cb87f08d7df904f7c525bd1a6bf5cd05e441f31
Abdul Haque <132687150+AbdulHaq03@users.noreply.github.com>
2024-05-04 22:16:34 +0500

    Changed image url to image src from internet

--- src/components/heroSection/HeroSection.jsx
+++ src/components/heroSection/HeroSection.jsx
@@ -45,7 +45,7 @@ const HeroSection = () => {
         <div className="overflow-hidden">
             <Slider {...settings} style={{ height: sliderHeight }}>
                 <div>
-                    <img src="src/img/hero_1.jpg" alt="Hero 1" style={imageStyle} />
+                    <img src="https://images.unsplash.com/photo-1531297484001-80022131f5a1?q=80&w=2020&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hero 1" style={imageStyle} />
                 </div>
                 <div>
                     <img src="src/img/hero_2.jpg" alt="Hero 2" style={imageStyle} />
